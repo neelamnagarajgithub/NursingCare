7747b8114478d77ccb0b12d1dec3547288afd18d
Rithwik3425 <rithwikchimmani3425@gmail.com>
2024-01-31 21:27:04 +0530

    Completing the statc part

--- Client/src/Components/PageNav.jsx
+++ Client/src/Components/PageNav.jsx
@@ -3,10 +3,50 @@ function PageNav() {
     <div className="navbar">
       <h1>Nursing Care</h1>
       <ul className="navelements">
-        <li className="navele">Home</li>
-        <li className="navele">Nursing</li>
-        <li className="navele">About us</li>
-        <li className="navele">Contact us</li>
+        <li
+          className="navele"
+          onClick={(e) => {
+            e.preventDefault();
+            document
+              .getElementById("home")
+              .scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          Home
+        </li>
+        <li
+          className="navele"
+          onClick={(e) => {
+            e.preventDefault();
+            document
+              .getElementById("whyus")
+              .scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          Nursing
+        </li>
+        <li
+          className="navele"
+          onClick={(e) => {
+            e.preventDefault();
+            document
+              .getElementById("testimonials")
+              .scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          About us
+        </li>
+        <li
+          className="navele"
+          onClick={(e) => {
+            e.preventDefault();
+            document
+              .getElementById("contactus")
+              .scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          Contact us
+        </li>
         <button className="navbtn">Login</button>
       </ul>
     </div>
