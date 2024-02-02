791ceeeccf666f6faed0740bac39368354b2532c
Shatakshi <pallishatakshi@gmail.com>
2024-02-02 15:26:41 +0530

    footer-section

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -27,10 +27,22 @@ function Footer() {
         <div className="copyc">
           <div>copyright &copy; NursingCare 2024</div>
           <div className="footeicons">
-            <div className="facebook"></div>
-            <div className="linkedin"></div>
-            <div className="insta"></div>
-            <div className="twitter"></div>
+            <div>
+              <img src="https://i.pinimg.com/564x/70/b9/3a/70b93af70f637f2d8a2dec42bfcf23f8.jpg" className="facebook"
+              />
+            </div>
+            <div>
+            <img src="https://i.pinimg.com/564x/ba/72/3f/ba723fc8dfb5362956a680df14943be3.jpg" className="linkedin"
+              />
+            </div>
+            <div>
+            <img src="https://i.pinimg.com/736x/89/67/e5/8967e562321297cdfb75a50a6927beff.jpg" className="insta"
+              />
+            </div>
+            <div>
+            <img src="https://i.pinimg.com/564x/da/9c/c1/da9cc1f2e152683ddba2240817404977.jpg" className="twitter"
+              />
+            </div>
           </div>
         </div>
       </div>
